be6103b2788598f08bde49250799cf1102082482
levi <levicaseblodgett@gmail.com>
2019-11-14 15:13:43 -0800

    Classes assignment part 2

---
+++ classes2_assignment/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace classes2_assignment
+{
+    class Class1
+    {
+        /*constructor for class*/
+        public Class1(int num1, int num2)
+        {
+            Number1 = num1;
+            Number2 = num2;
+        }
+
+        /*properties*/
+        public int Number1 { get; set; }
+        public int Number2 { get; set; }
+
+        /*methods*/
+        public void Math1(int num1, int num2)
+        {
+            int num3 = num1 + num2;
+            Console.Write(num1 + " + " + num2 + " = " + num3);
+        }
+    }
+}
---
+++ classes2_assignment/classes2_assignment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classes2_assignment
+{
+    class Program
+    {
+        static void Main()
+        {
+            /*user enters a number*/
+            Console.Write("Enter a number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            /*user enters a second number*/
+            Console.Write("Enter another number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Class1 class1Instance = new Class1(num1, num2);
+
+            class1Instance.Math1(class1Instance.Number1, class1Instance.Number2);
+
+            Console.Read();
+        }
+    }
+}
