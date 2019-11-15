4fecce464d7f2d121c6f18d417009b1d0f81df39
levi <levicaseblodgett@gmail.com>
2019-11-15 15:48:36 -0800

    Classes assignment part 5 (overloaded method and output parameters)

---
+++ classes5_assignment/classes5_assignment/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace classes5_assignment
+{
+	public class Class1
+	{
+        /*method that divides a number in 2*/
+        public void Divide(int num)
+        {
+            int result = num / 2;
+            Console.WriteLine(num + " / 2 = " + result);
+        }
+
+        /*overloaded Divide method*/
+        /*this method takes in two numbers, and divides the first by the second*/
+        public void Divide(int num1, int num2)
+        {
+            int result = num1 / num2;
+            Console.WriteLine(num1 + " / " + num2 + " = " + result);
+        }
+	}
+}
---
+++ classes5_assignment/classes5_assignment/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classes5_assignment
+{
+    class Program
+    {
+        static void Main()
+        {
+            /*creates an instance of Class1*/
+            Class1 class1Instance = new Class1();
+
+            /*user enters a number*/
+            Console.Write("Enter a number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            /*calls the method that divides the user-defined number by 2*/
+            class1Instance.Divide(num1);
+
+            /*user enters a second number*/
+            Console.Write("Enter a second number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            /*calls the method that divides the first user-defined number by the second*/
+            class1Instance.Divide(num1, num2);
+
+            /*creates an undefined integer and passes it to Class2's Double method*/
+            int num3;
+            Class2.Double(out num3);
+
+            Console.Read();
+        }
+
+        /*static class*/
+        public static class Class2
+        {
+            /*method that takes in an out parameter*/
+            public static void Double(out int num)
+            {
+                num = 5;
+                int result = num * 2;
+                Console.WriteLine(num + " * 2 = " + result);
+            }
+        }
+    }
+}
