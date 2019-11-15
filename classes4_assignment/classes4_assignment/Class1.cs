92b8aefe138af33dc7c90418bd07b67675fcb09f
levi <levicaseblodgett@gmail.com>
2019-11-15 12:02:08 -0800

    Classes assignment part 4

---
+++ classes4_assignment/classes4_assignment/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace classes4_assignment
+{
+	public class Class1
+	{
+        public int Math1(int num1, int num2 = 0)
+        {
+            int num3 = 0;
+            if (num2 != 0)
+            {
+                num3 = num1 + num2;
+                Console.WriteLine(num1 + " + " + num2 + " = " + num3);
+            }
+            else
+            {
+                num3 = num1 + 0;
+                Console.WriteLine(num1 + " + 0 = " + num3);
+            }
+
+            return num3;
+        }
+	}
+}
---
+++ classes4_assignment/classes4_assignment/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classes4_assignment
+{
+    class Program
+    {
+        static void Main()
+        {
+            Class1 class1Instance = new Class1();
+
+            int num1;
+            int num2;
+
+            Console.Write("Enter a number: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("OPTIONAL: Enter a second number, if you want; if not, just press enter: ");
+            try
+            {
+                num2 = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                num2 = 0;
+            }
+
+            class1Instance.Math1(num1, num2);
+
+            Console.Read();
+        }
+    }
+}
