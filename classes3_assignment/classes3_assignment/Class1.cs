4752432bcb89f01436b34790b8ddd7264915f95d
levi <levicaseblodgett@gmail.com>
2019-11-14 18:15:12 -0800

    Classes assignment 3

---
+++ classes3_assignment/classes3_assignment/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+namespace classes3_assignment
+{
+	class Class1
+	{
+        /*methods*/
+        /*takes an int and divides it by 2 and returns the result*/
+        public int Halve(int num)
+        {
+            int result = num / 2;
+            return result;
+        }
+
+        /*takes a decimal, doubles it, and returns the result*/
+        public int Double(decimal num)
+        {
+            int result = (int)num * 2;
+            return result;
+        }
+
+        /*takes a string, converts it to an integer (if possible), multiplies it by itself, and returns the result*/
+        public int Square(string str)
+        {
+            try
+            {
+                int num = Convert.ToInt32(str);
+                num = num * num;
+                return num;
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return -1;
+            }
+        }
+	}
+}
---
+++ classes3_assignment/classes3_assignment/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classes3_assignment
+{
+    class Program
+    {
+        static void Main()
+        {
+            Class1 math1 = new Class1();
+
+            Console.WriteLine("12 / 2 = " + math1.Halve(12));
+            Console.WriteLine("8 * 2 = " + math1.Double(8));
+            Console.WriteLine("3 ^ 2 = " + math1.Square("3"));
+
+            /*tests the Square method by passing a non-number as a string*/
+            if (math1.Square("tr") == -1)
+            {
+                Console.WriteLine();
+            }
+
+            Console.Read();
+        }
+    }
+}
