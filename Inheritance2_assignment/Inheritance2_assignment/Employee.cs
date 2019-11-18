35e25fa5c3be5410fe968af1c68f7c860c44c35d
levi <levicaseblodgett@gmail.com>
2019-11-18 14:33:18 -0800

    Inheritance assignment part 2

---
+++ Inheritance2_assignment/Inheritance2_assignment/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Inheritance2_assignment
+{
+    /*class that is inherited from Person*/
+    public class Employee : Person
+    {
+        /*constructor for the inherited class*/
+        public Employee(string fname, string lname) : base(fname, lname)
+        {
+        }
+
+        /*override for Person's SayName function, which displays the full name of the person/employee*/
+        public override void SayName()
+        {
+            base.SayName();
+        }
+    }
+}
---
+++ Inheritance2_assignment/Inheritance2_assignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance2_assignment
+{
+    class Program
+    {
+        static void Main()
+        {
+            string fname = "Sample";
+            string lname = "Student";
+
+            /*creates an object of class Employee, which is derived from Person*/
+            Employee emp1 = new Employee(fname, lname);
+
+            emp1.SayName();
+
+            Console.Read();
+        }
+    }
+}
