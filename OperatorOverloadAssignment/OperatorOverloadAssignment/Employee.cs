a0ec0307ff39627e033ee96de8ec9d61f0d387a2
levi <levicaseblodgett@gmail.com>
2019-11-19 13:39:00 -0800

    Operator overload assignment

---
+++ OperatorOverloadAssignment/OperatorOverloadAssignment/Employee.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace OperatorOverloadAssignment
+{
+    /*class that is inherited from Person*/
+    public class Employee : Person
+    {
+        /*Properties*/
+        public int Id { get; set; }
+
+        /*constructor for the inherited class*/
+        public Employee(string fname, string lname, int ident) : base(fname, lname)
+        {
+            Id = ident;
+        }
+
+        /*override for Person's SayName function, which displays the full name of the person/employee*/
+        public override void SayName()
+        {
+            base.SayName();
+            Console.WriteLine("EMPLOYEE ID: " + Id + "\n");
+        }
+
+        /*overloads the == operator to compare two employees by Id number*/
+        public static bool operator== (Employee employee1, Employee employee2)
+        {
+            bool result;
+
+            if (employee1.Id == employee2.Id)
+            {
+                result = true;
+            }
+            else
+            {
+                result = false;
+            }
+
+            return result;
+        }
+
+        /*overloads the != to compare two employees by Id number*/
+        public static bool operator!= (Employee employee1, Employee employee2)
+        {
+            bool result;
+
+            if (employee1.Id != employee2.Id)
+            {
+                result = true;
+            }
+            else
+            {
+                result = false;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ OperatorOverloadAssignment/OperatorOverloadAssignment/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorOverloadAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*creates and defines strings for first employee*/
+            string fname1 = "Jimmy";
+            string lname1 = "TwoFeet";
+            /*creates and defines strings for second employee*/
+            string fname2 = "Johnny";
+            string lname2 = "LooseLips";
+
+            Employee emp1 = new Employee(fname1, lname1, 101);
+            Employee emp2 = new Employee(fname2, lname2, 101);
+
+            emp1.SayName();
+            emp2.SayName();
+
+            Console.WriteLine("\n");
+
+            if (emp1 == emp2)
+            {
+                Console.Write("Employee 1 and Employee 2 have the same ID number of: " + emp1.Id);
+            }
+            else if (emp1 != emp2)
+            {
+                Console.WriteLine("Employee 1 and 2 do NOT have the same ID number; They have ID numbers of: " + emp1.Id + " and " + emp2.Id);
+            }
+
+            Console.Read();
+        }
+    }
+}
