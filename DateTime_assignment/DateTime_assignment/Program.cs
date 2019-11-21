f2e75c0402014a1a6052d6030b78f2c50ede5171
levi <levicaseblodgett@gmail.com>
2019-11-21 12:15:01 -0800

    DateTime assignment

---
+++ DateTime_assignment/DateTime_assignment/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTime_assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*gets the current date and time*/
+            DateTime current = DateTime.Now;
+
+            /*user enters a number*/
+            Console.Write("Enter a number: ");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            /*creates a "DateTime" variable that is set for a number of hours past the current time, the number
+             * of hours being the number the user entered*/
+            DateTime untilThen = current.AddHours(input);
+
+            /*displays the current time and the speficied future time*/
+            Console.WriteLine("CURRENT: " + current);
+            Console.WriteLine("TIME IN " + input + " HOURS: " + untilThen);
+
+            Console.Read();
+        }
+    }
+}
