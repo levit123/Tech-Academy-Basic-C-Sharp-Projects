ac8affcb43aa1a6636dc20e51204fe925929b66c
levi <levicaseblodgett@gmail.com>
2019-11-21 13:53:25 -0800

    Var exercise

---
+++ Var_exercise/Var_exercise/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Var_exercise
+{
+    /*class that is inherited from Person*/
+    public class Employee : Person
+    {
+        /*Properties*/
+        public int Id { get; set; }
+
+        /*default constructor; uses constructor chaining*/
+        public Employee() : this("Sample", "Employee", 1)
+        {
+        }
+
+        /*constructor for the Employee class*/
+        public Employee(string fname, string lname, int ident) : base(fname, lname)
+        {
+            Id = ident;
+        }
+
+        /*override for Person's SayName function, which displays the info of the person/employee*/
+        public override void SayName()
+        {
+            /*displays the full name and employee ID*/
+            base.SayName();
+            Console.WriteLine("EMPLOYEE ID: " + Id);
+        }
+    }
+}
---
+++ Var_exercise/Var_exercise/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Var_exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*declares a constant*/
+            const string constantString = "Potato";
+
+            /*user enters anything, and it is saved to the var*/
+            Console.Write("Enter something here: ");
+            var var1 = Console.ReadLine();
+
+            /*creates a var and defines as the result of concatenating the constant and user-defined var together*/
+            var result = constantString + var1;
+            Console.WriteLine(result);
+
+            /*creates an employee that uses the default constructor, which uses constructor chaining (see "Employee.cs")*/
+            Employee emp1 = new Employee();
+            emp1.SayName();
+
+            Console.Read();
+        }
+    }
+}
